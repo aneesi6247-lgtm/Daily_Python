4ac0fb713e7ea1cbc6ce1a7f27eb24542e657da8
aneesi6247-lgtm <aneesi6247@gmail.com>
2026-02-18 16:55:55 +0100

    First time

---
+++ 17.02.2026.py
@@ -0,0 +1,15 @@
+#Write a program that:
+	#1.	Takes a sentence from user
+	#2.	Counts how many times each word appears
+	#3.	Prints result in dictionary form
+
+snt = str(input("Please Enter the string"))
+word = snt.split()                  # it will split the string into the words
+print(word)
+dic = {}
+for val in word:
+    if val in dic:
+        dic[val] += 1
+    else:
+        dic[val] = 1
+print(dic)
