1685b6539ed3a483b3dd2d1fc3badb14d1dc797f
aneesi6247-lgtm <aneesi6247@gmail.com>
2026-02-23 20:56:49 +0100

    new

--- 23.02.2026.py
+++ 23.02.2026.py
@@ -8,3 +8,10 @@ a = a - b
 print("a=:", a)
 print("b=:", b)
 
+# take a number from the user and print its int and floating part seperately. 
+
+num = float(input("Please enter a floating number"))
+int_part = int(num)
+float_part = num - int_part
+print("the int part is: ", int_part)
+print("The float part is: ", float_part)
