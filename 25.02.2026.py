113143f198c8027415993e756bd4b6780395306c
aneesi6247-lgtm <aneesi6247@gmail.com>
2026-02-26 15:07:51 +0100

    done

--- 25.02.2026.py
+++ 25.02.2026.py
@@ -45,3 +45,69 @@ sec = n % 10
 print(sec)
 n= n//10
 print(n)
+
+# write a function to return the number of digits of a number
+num = "23423423"
+cnt=0
+for val in num:
+    cnt+=1
+print(cnt)
+
+# write a programe to get all the numbers in between 1 to 100 which are divisible by 3 and 5
+
+for k in range(1,101,1):
+    if(k%3==0 and k%5==0):
+        print(k)
+# write a programe to continously get an input from the user and decide weather input is positive or negative and stop it only when the user write "Quite"
+ 
+while True:
+    inp = (input("Please enter a number:"))
+    if (inp=="quite"):
+        break
+    try:
+        inp1 = int(inp)
+        if (inp1>0):
+            print(" the value is positive")
+        elif inp1<0:
+            print("the number is negative")
+        else:
+            print("the number is zero")
+    except ValueError:
+            print("please enter a valid number or quite")
+
+# make a calculator
+
+def cal(a,b, operation):
+    if operation =="+":
+        print(a+b)
+    if operation =="-":
+        print(a-b)
+    if operation =="*":
+        print(a*b)
+
+cal(13,532,"+")
+
+#write a program to tell weather the number is prime or not
+
+get = int(input("Please enter the number:"))
+if get <1:
+    print("not a prime number")
+
+for val in range (2,get):
+    if get%val == 0:
+        print("not a prime number")
+        break
+    else:
+        print("IS PRIME NUMBER")
+        break
+# write a code to print all the prime numbers from 1 to 100
+
+n = 100
+for val in range (2,n+1):
+    is_prime = True
+    for i in range(2,val):
+        if val % i == 0:
+            is_prime= False
+            break
+    if (is_prime):
+        print(val)
